afcabaa545ed4db367260005e6e87102a0ef2306
sunsidew <danielay@naver.com>
2014-07-20 15:11:41 +0900

    simple refactoring end

--- jsonManager.m
+++ jsonManager.m
@@ -22,37 +22,39 @@
 
 - (NSObject*) MYJSONSerializationFrom:(NSString *)jsonString {
 
-    NSMutableString* sample = [jsonString substringWithRange:NSMakeRange(1, jsonString.length-2)]; // trim;
     NSString* selector = [jsonString substringToIndex:1]; // array, dictionary 구분
+    NSMutableString* sample = [jsonString substringWithRange:NSMakeRange(1, jsonString.length-2)]; // trim;
     
     if ([selector isEqualToString: @("{")] || [selector isEqualToString: @("[")]) {
-        NSMutableDictionary * divdict = [[NSMutableDictionary alloc] init];
-        NSMutableArray * divarray = [[NSMutableArray alloc] init];
         
         int level = 0;
         int f_idx = 0;
-
-        NSObject* key = [[NSObject alloc] init];
-        NSObject* value = [[NSObject alloc] init];
-        NSObject* object = [[NSObject alloc] init];
         NSString* element = [[NSString alloc] init];
         
+        NSMutableDictionary * divdict = [[NSMutableDictionary alloc] init];
+        NSObject* key = [[NSObject alloc] init];
+        NSObject* value = [[NSObject alloc] init];
+
+        NSMutableArray * divarray = [[NSMutableArray alloc] init];
+        NSObject* object = [[NSObject alloc] init];
+        char idxchar;
+        
         for ( int idx = 0 ; idx < sample.length ; idx++) {
-            if ([sample characterAtIndex:idx] == '{' || [sample characterAtIndex:idx] == '[') {
+            idxchar = [sample characterAtIndex:idx];
+            if (idxchar == '{' || idxchar == '[') {
                 level++;
-            } else if ([sample characterAtIndex:idx] == '}' || [sample characterAtIndex:idx] == ']') {
+            } else if (idxchar == '}' || idxchar == ']') {
                 level--;
-            } else if ([sample characterAtIndex:idx] == ',' && level == 0) {
+            } else if (idxchar == ',' && level == 0) {
+                element = [sample substringWithRange:NSMakeRange(f_idx,idx-f_idx)];
+                
                 if ([selector isEqualToString: @("{")]) {
-                    
-                    element = [sample substringWithRange:NSMakeRange(f_idx,idx-f_idx)];
-                                
                     key = [self MYJSONSerializationFrom:[element substringWithRange:NSMakeRange(0,[element rangeOfString: @":"].location)]];
                     value = [self MYJSONSerializationFrom:[element substringFromIndex:[element rangeOfString: @":"].location+1]];
                 
                     [divdict setObject:value forKey:key];
                 } else {
-                    object = [self MYJSONSerializationFrom:[sample substringWithRange:NSMakeRange(f_idx,idx-f_idx)]];
+                    object = [self MYJSONSerializationFrom:element];
                     
                     [divarray addObject:object];
                 }
@@ -60,6 +62,8 @@
                 f_idx = idx+1;
             }
         }
+        
+        // 마지막 element 삽입 작업
         
         element = [sample substringFromIndex:f_idx];
         
@@ -85,10 +89,11 @@
 
 - (NSString*) MYJSONMakerWithArray:(NSArray*)array {
     NSMutableString* result = [[NSMutableString alloc] init];
+    NSObject *object = [[NSObject alloc] init];
     
     [result appendString: @"["];
     for (int i = 0 ; i < [array count] ; i++) {
-        NSObject* object = array[i];
+        object = array[i];
         
         if ([object isKindOfClass:NSArray.class]) {
             object = [self MYJSONMakerWithArray:(NSArray*) object];
@@ -111,11 +116,11 @@
 
 - (NSString*) MYJSONMakerWithDictionary:(NSDictionary*)dictionary {
     NSMutableString *result = [[NSMutableString alloc] init];
-    //NSObject *object = [[NSObject alloc] init];
+    NSObject *object = [[NSObject alloc] init];
     
     [result appendString: @"{"];
     for (id key in dictionary) {
-        NSObject* object = [dictionary objectForKey:key];
+        object = [dictionary objectForKey:key];
         
         if ([object isKindOfClass:NSArray.class]) {
             object = [self MYJSONMakerWithArray:(NSArray*) object];
